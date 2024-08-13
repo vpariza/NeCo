a4db5d75a2cce9ac6af9dc3fd2abb337c00dacb7
Valentinos Pariza <vpariz01@turing1.ucy.ac.cy>
2024-08-14 00:39:52 +0300

    Webpage of neco

--- data/imagenet/__init__.py
+++
@@ -1,5 +0,0 @@
-import logging
-
-logger = logging.getLogger("im100kdata")
-logger.addHandler(logging.StreamHandler())
-logger.setLevel(logging.INFO)
--- src/layers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .dino_head import DINOHead
-from .mlp import Mlp
-from .patch_embed import PatchEmbed
-from .swiglu_ffn import SwiGLUFFN, SwiGLUFFNFused
-from .block import NestedTensorBlock
-from .attention import MemEffAttention
